3d9d9acbb30c9a0320a3555d5a1bc230554541a3
Sandip <sandip.ghimire@mavorion.com>
2026-03-23 17:50:29 +0545

    feat: Implement animated user dropdown, refine sidebar items, update Dashboard view, and adjust main content padding.

--- app/frontend/src/layout/AuthLayout.tsx
+++ app/frontend/src/layout/AuthLayout.tsx
@@ -10,7 +10,7 @@ export default function AuthLayout() {
     <div className={`min-h-dvh bg-background flex flex-col`}>
       <Header></Header>
       <Sidebar></Sidebar>
-      <div className={`pt-20 transition-all duration-300 ease-in-out ${isSidebarOpen ? "md:pl-68" : "pl-4"}`}>
+      <div className={`pt-20 transition-all duration-300 ease-in-out pr-4 ${isSidebarOpen ? "md:pl-68 pl-4" : "pl-4"}`}>
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
